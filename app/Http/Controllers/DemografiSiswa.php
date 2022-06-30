5931ce19894f5f9b8eefa783a73556849acf7059
Ahmad Shaleh <ahmadsaleh2409@gmail.com>
2022-06-30 14:43:47 +0700

    update demografi

--- app/Http/Controllers/DemografiSiswa.php
+++ app/Http/Controllers/DemografiSiswa.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\DB;
 
 class DemografiSiswa extends Controller
 {
+    protected $title = 'Statistik Siswa';
+
     function getJumlahSiswaPerKelas($kelas){
         return DB::table('v_jadwal_siswa')
                 ->where('tingkat_kelas', $kelas)
@@ -27,26 +29,26 @@ class DemografiSiswa extends Controller
 
     public function semua(){
         $data = [
-            'title' => 'Demografi Siswa',
+            'title' => $this->title,
             'breadcrumb' => [
                 ['url' => '/' , 'name' => 'Home'],
                 ['url' => '/interface' , 'name' => 'Interface'],
                 ['url' => '/master' , 'name' => 'List Data'],
             ],
-            'testVariable' => 'Demografi Siswa',
+            'testVariable' => $this->title,
         ];
         return view('demografi.siswa.semua', $data);
     }
 
     public function tingkatKelas(){
         $data = [
-            'title' => 'Demografi Siswa',
+            'title' => $this->title,
             'breadcrumb' => [
                 ['url' => '/' , 'name' => 'Home'],
                 ['url' => '/interface' , 'name' => 'Interface'],
                 ['url' => '/master' , 'name' => 'List Data'],
             ],
-            'testVariable' => 'Demografi Siswa'
+            'testVariable' => $this->title
         ];
 
         $data['kelasX'] = $this->getJumlahSiswaPerKelas('X');
@@ -60,52 +62,52 @@ class DemografiSiswa extends Controller
     
     public function tahunMasuk(){
         $data = [
-            'title' => 'Demografi Siswa',
+            'title' => $this->title,
             'breadcrumb' => [
                 ['url' => '/' , 'name' => 'Home'],
                 ['url' => '/interface' , 'name' => 'Interface'],
                 ['url' => '/master' , 'name' => 'List Data'],
             ],
-            'testVariable' => 'Demografi Siswa'
+            'testVariable' => $this->title
         ];
         return view('demografi.siswa.tahunmasuk', $data);
     }
 
     public function jenisKelamin(){
         $data = [
-            'title' => 'Demografi Siswa',
+            'title' => $this->title,
             'breadcrumb' => [
                 ['url' => '/' , 'name' => 'Home'],
                 ['url' => '/interface' , 'name' => 'Interface'],
                 ['url' => '/master' , 'name' => 'List Data'],
             ],
-            'testVariable' => 'Demografi Siswa'
+            'testVariable' => $this->title
         ];
         return view('demografi.siswa.jeniskelamin', $data);
     }
 
     public function agama(){
         $data = [
-            'title' => 'Demografi Siswa',
+            'title' => $this->title,
             'breadcrumb' => [
                 ['url' => '/' , 'name' => 'Home'],
                 ['url' => '/interface' , 'name' => 'Interface'],
                 ['url' => '/master' , 'name' => 'List Data'],
             ],
-            'testVariable' => 'Demografi Siswa'
+            'testVariable' => $this->title
         ];
         return view('demografi.siswa.agama', $data);
     }
 
     public function statusSiswa(){
         $data = [
-            'title' => 'Demografi Siswa',
+            'title' => $this->title,
             'breadcrumb' => [
                 ['url' => '/' , 'name' => 'Home'],
                 ['url' => '/interface' , 'name' => 'Interface'],
                 ['url' => '/master' , 'name' => 'List Data'],
             ],
-            'testVariable' => 'Demografi Siswa'
+            'testVariable' => $this->title
         ];
         return view('demografi.siswa.statussiswa', $data);
     }
